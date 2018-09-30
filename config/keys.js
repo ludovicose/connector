d6b37742ffa7355455d5150a59042b90754249f5
Aibek Muratbekov <aibek.muratbekov@gmail.com>
2018-09-30 21:55:30 +0600

    Passport User object

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://connector:35h1cjmjr@ds253831.mlab.com:53831/connector'
+    mongoURI: 'mongodb://connector:35h1cjmjr@ds253831.mlab.com:53831/connector',
+    secretOrKey: 'secret'
 };
