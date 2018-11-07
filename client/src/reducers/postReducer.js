722ea567707b3226fce1837e70f4c13eb167b88a
Aibek Muratbekov <aibek.muratbekov@gmail.com>
2018-11-07 20:57:31 +0600

    POST STATE adddPost Action

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,13 @@
+const initailState = {
+    posts: [],
+    post: {},
+    loading: false
+};
+
+export default function (state = initailState, action) {
+
+    switch (action.type){
+        default:
+            return state;
+    }
+}
